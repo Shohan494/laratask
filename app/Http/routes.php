7a798c665f013447ffe1ce4994b052386df9a1c2
Shohanul Alam <shohan12224943@gmail.com>
2015-11-25 06:35:26 +0600

    validation of adding a task
    when user wants to add a task, the function validates it and if anything
    is wrong then error occurs

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,7 +14,17 @@ Route::get('/', function () {
  * Add A New Task
  */
 Route::post('/task', function (Request $request) {
-    //
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    // Create The Task...
 });
 
 /**
