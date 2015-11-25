c40eddc9140dfa8fabb96f88a6a4d52d080fb924
Shohanul Alam <shohan12224943@gmail.com>
2015-11-25 07:03:49 +0600

    showing the tasks in table

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -9,7 +9,7 @@
         @include('common.errors')
 
         <!-- New Task Form -->
-        <form action="/task" method="POST" class="form-horizontal">
+        <form action={{url("/task")}} method="POST" class="form-horizontal">
             {{ csrf_field() }}
 
             <!-- Task Name -->
@@ -33,4 +33,40 @@
     </div>
 
     <!-- TODO: Current Tasks -->
+        <!-- Current Tasks -->
+    @if (count($tasks) > 0)
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Current Tasks
+            </div>
+
+            <div class="panel-body">
+                <table class="table table-striped task-table">
+
+                    <!-- Table Headings -->
+                    <thead>
+                        <th>Task</th>
+                        <th>&nbsp;</th>
+                    </thead>
+
+                    <!-- Table Body -->
+                    <tbody>
+                        @foreach ($tasks as $task)
+                            <tr>
+                                <!-- Task Name -->
+                                <td class="table-text">
+                                    <div>{{ $task->name }}</div>
+                                </td>
+
+                                <td>
+                                    <!-- TODO: Delete Button -->
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    @endif
+@endsection
 @endsection
