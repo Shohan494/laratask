cab86af0894397a9d5e62862356b73ce1b905317
Shohanul Alam <shohan12224943@gmail.com>
2015-11-25 06:20:42 +0600

    routes has been declared and root view was set

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,25 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+use App\Task;
+use Illuminate\Http\Request;
 
+/**
+ * Display All Tasks
+ */
 Route::get('/', function () {
-    return view('welcome');
+    return view('tasks');
+});
+
+/**
+ * Add A New Task
+ */
+Route::post('/task', function (Request $request) {
+    //
+});
+
+/**
+ * Delete An Existing Task
+ */
+Route::delete('/task/{id}', function ($id) {
+    //
 });
